d3dd8b963004275ac4ec0f0daab781f25734f1e1
Yulia Tsareva <yulia.tcareva@gmail.com>
2016-01-26 01:52:38 +0300

    Reuse webpack.config in webpack.prod.config

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,68 +1,14 @@
-var path = require('path');
 var webpack = require('webpack');
+var webpackConfig = require('./webpack.config');
 
-module.exports = {
-    debug: false,
+webpackConfig.devtool = null;
+webpackConfig.debug = false;
 
-    // our angular app
-    entry: {'vendor': './app/vendor.ts', 'main': './app/main.ts'},
-
-    // Config for our build files
-    output: {
-        path: path.resolve('dist'),
-        filename: '[name].bundle.js',
-        chunkFilename: '[id].chunk.js'
-    },
-
-    resolve: {
-        // ensure loader extensions match
-        extensions: ['', '.ts', '.js']
-    },
-
-    module: {
-        preLoaders: [{test: /\.ts$/, loader: 'tslint-loader', exclude: [/node_modules/]}],
-        loaders: [
-            {
-                test: /\.ts$/,
-                loader: 'ts-loader',
-                exclude: [/\.(spec|e2e)\.ts$/, /node_modules\/(?!(ng2-.+))/]
-            },
-            {
-                test: /\.csv?$/,
-                loader: 'dsv-loader'
-            }
-        ]
-    },
-
-    plugins: [
-        new webpack.optimize.OccurenceOrderPlugin(true),
-        new webpack.optimize.CommonsChunkPlugin({name: 'vendor', filename: 'vendor.bundle.js', minChunks: Infinity}),
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false
-            }
-        })
-    ],
-
-    // Other module loader config
-    tslint: {
-        emitErrors: false,
-        failOnHint: false
-    },
-    // our Webpack Development Server config
-    //devServer: {
-    //    port: metadata.port,
-    //    host: metadata.host,
-    //    historyApiFallback: true,
-    //    watchOptions: { aggregateTimeout: 300, poll: 1000 }
-    //},
-    // we need this due to problems with es6-shim
-    node: {
-        global: 'window',
-        progress: false,
-        crypto: 'empty',
-        module: false,
-        clearImmediate: false,
-        setImmediate: false
+var uglifyPlugin = new webpack.optimize.UglifyJsPlugin({
+    compress: {
+        warnings: false
     }
-};
+});
+webpackConfig.plugins.push(uglifyPlugin);
+
+module.exports = webpackConfig;
