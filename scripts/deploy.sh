d3e3d242a9df6b173c5ebc98a977ff162ec36b75
Yulia Tsareva <yulia.tcareva@gmail.com>
2016-01-27 04:08:48 +0300

    Test deploy with Travis CI

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -e # exit with nonzero exit code if anything fails
 
-# go to the out directory and create a *new* Git repo
-cd dist
+# copy files to the deploy directory
+mkdir deploy
+cp ./index.html ./deploy/index.html
+cp -r ./dist ./deploy/dist
+
+# go to the deploy directory and create a *new* Git repo
+cd deploy
 git init
 
 # inside this git repo we'll pretend to be a new user
@@ -12,7 +17,6 @@ git config user.email "yulia.tcareva@gmail.com"
 # The first and only commit to this new Git repo contains all the
 # files present with the commit message "Deploy to GitHub Pages".
 git add .
-#git add index.html
 git commit -m "Deploy to GitHub Pages"
 
 # Force push from the current repo's master branch to the remote
