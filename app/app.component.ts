21bee09f668e6c2174a0186f6310dbdb2c548f81
Yulia Tsareva <yulia.tcareva@gmail.com>
2016-01-31 15:27:51 +0300

    Angular Router. Divide app.component into quiz.component and theory.component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,71 +1,21 @@
 import {Component} from 'angular2/core';
+import {RouterOutlet} from 'angular2/router';
+import {RouteConfig} from 'angular2/router';
+import {ROUTER_DIRECTIVES} from 'angular2/router';
 
-import {TaskService} from './task.service';
-import {ProgressComponent} from './progress.component';
-import {TaskComponent} from './task.component';
-import {ResultComponent} from './result.component';
+import {QuizComponent} from './quiz.component';
 import {TheoryComponent} from './theory.component';
 
 @Component({
     selector: 'my-app',
-    directives: [ProgressComponent, TaskComponent, ResultComponent, TheoryComponent],
-    template: `<a *ngIf="!theoryIsShown" href class="help" (click)="toggleTheory($event)">
-                 <i class="fa fa-question-circle"></i>Tabla de verbos
-               </a>
-               <a *ngIf="theoryIsShown" href class="help" (click)="toggleTheory($event)">
-                 <i class="fa fa-times"></i>Cerrar la tabla
-               </a>
-               <div *ngIf="store.task">
-                 <my-progress [max]="store.totalCount" [value]="store.doneCount"></my-progress>
-                 <my-task>
-                 </my-task>
-               </div>
-               <my-result *ngIf="store.noMoreQuestions()"></my-result>
-               <my-theory *ngIf="theoryIsShown"></my-theory>`,
-    styles: [`
-        my-task {
-            display: block;
-            padding: 5px 0;
-            margin-top: 115px;
-        }
-        .help {
-            position: absolute;
-            top: 20px;
-            right: 20px;
-            font-size: 18px;
-        }
-        .fa {
-            margin-right: 5px;
-        }
-        my-theory {
-            position: absolute;
-            background-color: white;
-            top: 50px;
-            bottom: 0px;
-            left: 0px;
-            right: 0px;
-            -webkit-column-count: 3;
-            -moz-column-count: 3;
-            column-count: 3;
-            font-size: 15px;
-            line-height: 24px;
-            padding: 20px 15px 10px 40px;
-        }
-        @media (max-width: 767px) {
-            my-task {
-                margin-top: 60px;
-            }
-        }`
-    ]
+    directives: [RouterOutlet, ROUTER_DIRECTIVES],
+    template: `
+      <router-outlet></router-outlet>
+    `
 })
+@RouteConfig([
+    {path: '/', name: 'Quiz', component: QuizComponent},
+    {path: '/theory', name: 'Theory', component: TheoryComponent}
+])
 export class AppComponent {
-    private theoryIsShown: boolean;
-
-    constructor(public store: TaskService) {
-    }
-
-    private toggleTheory(event) {
-        this.theoryIsShown = !this.theoryIsShown;
-        event.preventDefault();
-    }
 }
--- app/theory.component.ts
+++ app/theory.component.ts
@@ -1,10 +1,16 @@
 import {Component} from 'angular2/core';
+import {ROUTER_DIRECTIVES} from 'angular2/router';
 
 import {TaskService} from './task.service';
 
 @Component({
     selector: 'my-theory',
-    template: `<ul>
+    directives: [ROUTER_DIRECTIVES],
+    template: `
+               <a href class="help" [routerLink]="['Quiz']">
+                 <i class="fa fa-times"></i>Cerrar la tabla
+               </a>
+               <ul>
                  <li *ngFor="#verb of store.verbs">{{verb.verb}} <strong>{{verb.preposition}}</strong> {{verb.object}}
                  </li>
                </ul>
