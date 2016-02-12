920587a6369856e119fe0980f4e252f96388a277
Yulia Tsareva <yulia.tcareva@gmail.com>
2016-02-12 23:56:55 +0300

    Remove unused code

--- webpack.test.config.js
+++ webpack.test.config.js
@@ -84,15 +84,3 @@ module.exports = {
         setImmediate: false
     }
 };
-
-// Helper functions
-
-function root(args) {
-    args = Array.prototype.slice.call(arguments, 0);
-    return path.join.apply(path, [__dirname].concat(args));
-}
-
-function rootNode(args) {
-    args = Array.prototype.slice.call(arguments, 0);
-    return root.apply(path, ['node_modules'].concat(args));
-}
