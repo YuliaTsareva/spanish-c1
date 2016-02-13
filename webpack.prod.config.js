3eff918e6f319b2b4301c739299eed400f0c400f
Yulia Tsareva <yulia.tcareva@gmail.com>
2016-02-14 01:19:24 +0300

    webpack-dev-server

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,15 +1,24 @@
 var webpack = require('webpack');
 var webpackConfig = require('./webpack.config');
+var ENV = process.env.ENV = 'production';
 
 webpackConfig.devtool = null;
 webpackConfig.debug = false;
 
-var uglifyPlugin = new webpack.optimize.UglifyJsPlugin({
-    compress: {
-        warnings: false
-    },
-    mangle: false
-});
-webpackConfig.plugins.push(uglifyPlugin);
+webpackConfig.plugins = [
+    new webpack.optimize.OccurenceOrderPlugin(true),
+    new webpack.optimize.CommonsChunkPlugin({name: 'vendor', filename: 'vendor.bundle.js', minChunks: Infinity}),
+    new webpack.DefinePlugin({
+        'process.env': {
+            'ENV': JSON.stringify(ENV)
+        }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+        compress: {
+            warnings: false
+        },
+        mangle: false
+    })
+];
 
 module.exports = webpackConfig;
