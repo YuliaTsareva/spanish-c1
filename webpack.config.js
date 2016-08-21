8f2a4a4aa2441def90c18f6df3d96af3d96c8443
Yulia Tsareva <yulia.tcareva@gmail.com>
2016-08-21 13:01:31 +0300

    Fix unit tests. Move config files to separate folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,91 +1,21 @@
-var path = require('path');
-var webpack = require('webpack');
-var ENV = process.env.ENV = 'development';
+/**
+ * @author: @AngularClass
+ */
 
-module.exports = {
-    // for faster builds use 'eval'
-    devtool: 'eval',
-    debug: true,
+console.log('process.env.NODE_ENV', process.env.NODE_ENV);
 
-    // our angular app
-    entry: {
-        'polyfills': './app/polyfills.browser.ts',
-        'vendor':    './app/vendor.browser.ts',
-        'main':      './app/main.browser.ts',
-        'test':      './app/test-main.js'
-    },
-
-    // Config for our build files
-    output: {
-        path: path.resolve('dist'),
-        filename: '[name].bundle.js',
-        sourceMapFilename: '[name].map',
-        chunkFilename: '[id].chunk.js',
-        publicPath: 'dist'
-    },
-
-    resolve: {
-        // ensure loader extensions match
-        extensions: ['', '.ts', '.js']
-    },
-
-    module: {
-        preLoaders: [
-            {
-                test: /\.ts$/,
-                loader: 'tslint-loader',
-                exclude: [/node_modules/]
-            }
-        ],
-        loaders: [
-            {
-                test: /\.ts$/,
-                loader: 'awesome-typescript-loader',
-                exclude: [/node_modules\/(?!(ng2-.+))/]
-            },
-            {
-                test: /\.csv$/,
-                loader: 'dsv-loader'
-            },
-            {
-                test: /\.css$/,
-                loader: 'raw-loader'
-            }
-        ]
-    },
-
-    plugins: [
-        new webpack.optimize.OccurenceOrderPlugin(true),
-        // new webpack.optimize.CommonsChunkPlugin({name: 'vendor', filename: 'vendor.bundle.js', minChunks: Infinity}),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: ['polyfills', 'vendor'].reverse()
-        }),
-        new webpack.DefinePlugin({
-            'process.env': {
-                'ENV': JSON.stringify(ENV)
-            }
-        })
-    ],
-
-    // Other module loader config
-    tslint: {
-        emitErrors: false,
-        failOnHint: false
-    },
-    // our Webpack Development Server config
-    //devServer: {
-    //    port: metadata.port,
-    //    host: metadata.host,
-    //    historyApiFallback: true,
-    //    watchOptions: { aggregateTimeout: 300, poll: 1000 }
-    //},
-    // we need this due to problems with es6-shim
-    node: {
-        global: 'window',
-        progress: false,
-        crypto: 'empty',
-        module: false,
-        clearImmediate: false,
-        setImmediate: false
-    }
-};
+// Look in ./config folder for webpack.dev.js
+switch (process.env.NODE_ENV) {
+    case 'prod':
+    case 'production':
+        module.exports = require('./config/webpack.prod.config');
+        break;
+    case 'test':
+    case 'testing':
+        module.exports = require('./config/webpack.test.config');
+        break;
+    case 'dev':
+    case 'development':
+    default:
+        module.exports = require('./config/webpack.config');
+}
